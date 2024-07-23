26d965d1b26d4c46f1a9abf94ccc58b04150dad6
Atul kumar Kashyap <1234atulkashyap@gmail.com>
2024-07-23 17:21:26 +0530

    completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'customer Page',
+};
+
 export default function Customer() {
     return <p>Customers Page</p>;
   }
